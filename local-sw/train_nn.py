648a1bc743e6a702b700b072c84f446d10b9aa62
Dan Guest <dguest@cern.ch>
2018-05-17 12:12:21 +0200

    Save the variable configuration with NN
    The train_nn.py script now saves information about the input variable
    normalization when it runs. This is enough information in to run the
    lwtnn kerasfunc2json.py script so that we can port the network back to
    ATLAS code.

--- local-sw/train_nn.py
+++ local-sw/train_nn.py
@@ -21,6 +21,10 @@ def get_args():
     parser.add_argument('-o','--output-dir', default='model')
     return parser.parse_args()
 
+###############################################################
+# Main Training Part
+###############################################################
+
 def run():
     args = get_args()
 
@@ -55,6 +59,17 @@ def run():
         arch_file.write(model.to_json(indent=2))
     model.save_weights(f'{odir}/weights.h5')
 
+    # also write out the variable specification
+    with open(f'{odir}/variables.json', 'w') as vars_file:
+        json.dump(get_variables_json(), vars_file)
+
+
+def make_targets(jets):
+    labels = jets['HadronConeExclExtendedTruthLabelID']
+    targets = np.stack([labels == x for x in [0, 4, 5]], axis=1)
+    return np.asarray(targets, dtype=int)
+
+
 def get_discrim(jets, model_path, weights_path):
     """
     Get the discriminant from a saved model
@@ -73,23 +88,93 @@ def get_discrim(jets, model_path, weights_path):
     output_log_ratio = np.log(b_weight / light_weights)
     return output_log_ratio
 
+
+
+####################################################################
+# Preprocessing and saving the configuration
+####################################################################
+#
+# These constants are used to normalize the NN inputs. We typically
+# want inputs that are around zero with standard deviations around 1,
+# but this doesn't need to be exact.
+JF_OFFSET = -0.7
+JF_SCALE = 0.75
+RNN_OFFSET = 3.0
+RNN_SCALE = 0.25
+RNN_DEFAULT = -9
+
 def preproc_inputs(jets):
     """
     We make some hardcoded transformations to normalize these inputs
-    they don't have to be exact.
     """
-    jf = (np.log1p(jets['jf_sig']) - 0.7 ) * 0.75
+
+    # The jf_sig variable has a long tail. To "normalize" this a bit
+    # we take the log(x + 1) transformation.
+    jf = (np.log1p(jets['jf_sig']) + JF_OFFSET ) * JF_SCALE
     print('jf mean: {}, std: {}'.format(np.mean(jf), np.std(jf)))
+
+    # the rnnip ratio has the weird feature that the outputs are
+    # sometimes NAN (because the algorithm returns zeros for all
+    # classes when no tracks are found). We have to replace these nan
+    # values with some other default
     rnnip = jets['rnnip_log_ratio']
-    rnnip[np.isnan(rnnip)] = -9
-    rnnip = (rnnip + 3.0) * 0.25
+    rnnip[np.isnan(rnnip)] = RNN_DEFAULT
+    rnnip = (rnnip + RNN_OFFSET) * RNN_SCALE
     print('rnnip mean: {}, std: {}'.format(np.mean(rnnip), np.std(rnnip)))
     return np.stack([jf, rnnip], axis=1)
 
-def make_targets(jets):
-    labels = jets['HadronConeExclExtendedTruthLabelID']
-    targets = np.stack([labels == x for x in [0, 4, 5]], axis=1)
-    return np.asarray(targets, dtype=int)
+def get_variables_json():
+    """
+    Make a file that specifies the input variables and
+    transformations as JSON, so that the network can be used with lwtnn
+    """
+
+    # This is a more 'traditional' network with one set of inputs so
+    # we just have to name the variables in one input node. In more
+    # advanced cases we could have multiple input nodes, some of which
+    # might operate on sequences.
+    btag_variables = [
+        {
+            # Note this is not the same name we use in the file! We'll
+            # have to make the log1p transformation in the C++ code to
+            # build this variable.
+            'name': 'jf_sig_log1p',
+            'offset': JF_OFFSET,
+            'scale': JF_SCALE,
+        },
+        {
+            'name': 'rnnip_log_ratio',
+            'offset': RNN_OFFSET,
+            'scale': RNN_SCALE,
+            'default': RNN_DEFAULT,
+        }
+    ]
+
+    # note that this is a list of output nodes, where each node can
+    # have multiple output values. In principal we could also add a
+    # regression output to the same network (i.e. the b-hadron pt) but
+    # that's a more advanced subject.
+    outputs = [
+        {
+            'name': 'classes',
+            'labels': ['light', 'charm', 'bottom']
+        }
+    ]
+
+    # lwtnn expects a specific format that allows multiple input and
+    # output nodes, so we have to dress the above information a bit.
+    final_dict = {
+        'input_sequences': [],
+        'inputs': [
+            {
+                'name': 'btag_variables',
+                'variables': btag_variables
+            }
+        ],
+        'outputs': outputs
+    }
+
+    return final_dict
 
 
 
